80cef648fe8b1806af8db8597e3fef074763a9c3
John Otander <johnotander@gmail.com>
2015-12-02 15:29:57 -0700

    Add spec and initial base class

--- lib/email_trail.rb
+++ lib/email_trail.rb
@@ -1,5 +1,16 @@
 require "email_trail/version"
 
 module EmailTrail
-  # Your code goes here...
+  class Base
+
+    def self.delivering_email(message)
+      to = message.to.to_s
+      subject = message.subject.to_s
+      message = message.body.to_s
+    end
+  end
 end
+
+defined?(ActionMailer::Base) ?
+  ActionMailer::Base.register_interceptor(EmailTrail::Base) :
+  fail('email_trail requires ActionMailer::Base')
